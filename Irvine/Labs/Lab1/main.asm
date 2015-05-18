8155b392c7d6d3a0e101c4fa18ad2d4bab088aaa
j <moraaljj@gmail.com>
2015-05-17 22:11:40 -0400

    Pushing my changes that are unfinished so bryan and i can work from the same base.

--- Irvine/Labs/Lab1/main.asm
+++ Irvine/Labs/Lab1/main.asm
@@ -1,11 +1,14 @@
 INCLUDE Irvine32.inc
 
 .data
-filename byte   "input.txt", 0
-written  dword  0
-message  byte   256 dup(0)
-len      dword  0
-capacity dword  256
+filename byte  "input.txt", 0
+written  dword 0
+message  byte  256 dup(0)
+numbers  dword 256 dup(?)
+capacity dword 256
+index    dword ?
+saved    dword ?
+len      dword ?
 
 file     handle ?
 stdout   handle ?
@@ -17,6 +20,34 @@ FILE_CREATE_ERROR byte "Error - Failed to create input file.", 0dh, 0ah, 0
 .code
 sort proc
 sort endp
+
+; @registers
+;   eax: start index
+;   ebx: end index
+;   edx: string offset
+; @return: integer value of string
+atoi proc
+    push ebp
+    mov ebp, esp
+    sub esp, 12
+    mov [ebp - 4], eax  ; local storage of start
+    mov [ebp - 8], ebx  ; local storage of end
+    mov [ebp - 12], edx ; local storage of string offset
+
+    mov eax, 0   ; accumulator for integer value
+    mov ecx, ebx ; current index between start and end
+
+atoi_loop:
+    mov ebx, [ebp - 12]
+    add ebx, ecx
+    mov edx, [ebx]
+
+    dec ecx
+    cmp ecx, [ebp - 8]
+    jne atoi_loop
+
+    pop ebp
+atoi endp
 
 main proc
     ; open input file
@@ -46,15 +77,35 @@ file_read:
     mov edx, offset message
     call WriteString
 
+    ; parse through input
+    mov index, 0
+    mov saved, 0
+parse_loop:
+    mov ebx, offset message
+    add ebx, index
+    mov eax, [ebx]
+    cmp eax, " "
+    jne end_parse_loop
+    mov eax, saved
+    mov ebx, index
+    mov edx, offset message
+    call atoi
+    mov ebx, index
+    mov saved, ebx
+
+end_parse_loop:
+    inc index
+    mov ebx, offset message
+    add ebx, index
+    mov eax, [ebx]
+    cmp eax, 0
+    jne parse_loop
+
     ; insert sorting here
 
-    ; write output to std out
-    invoke WriteConsole,
-        stdout,
-        addr message,
-        len,
-        addr written,
-        0
+    ; write sorted input to console
+    mov edx, offset message
+    call WriteString
 
     ; close input file
     mov eax, file
