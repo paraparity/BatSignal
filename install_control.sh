e8d5bd9a2e017dd6a2840a86bc1ad1ccf0b65ac5
teddy <moraaljj@gmail.com>
2015-08-08 18:29:41 +0000

    Finished control setup script.

--- install_control.sh
+++ install_control.sh
@@ -1,7 +1,7 @@
 cd setup/control
 
 # install dhcp
-apt-get install isc-dhcp-server
+apt-get install isc-dhcp-server < "Y"
 cp dhcp.conf /etc/dhcp/dhcp.conf
 
 # configure network interfaces
@@ -14,5 +14,8 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 # configure bat signal gateway
 cp bat-signal-gateway /etc/init.d/bat-signal-gateway
 update-rc.d bat-signal-gateway defaults
+
+# install dependencies for control node module
+apt-get install python3 pyhton3-dev < "Y"
 
 cd ../..
