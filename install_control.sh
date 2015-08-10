c3a0b319af244808ff4cf3cac8d7091c63b4e7df
YoungB2 <b.young1281@gmail.com>
2015-08-10 13:48:53 +0000

    Added flask installation to control install script.

--- install_control.sh
+++ install_control.sh
@@ -18,4 +18,20 @@ update-rc.d bat-signal-gateway defaults
 # install dependencies for control node module
 apt-get install python3 pyhton3-dev < "Y"
 
+# install python 3.4
+wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz
+tar -xvf Python-3.4.3.tgz
+cd Python-3.4.3
+./configure
+make
+make install
+
+wget https://bootstrap.pypa.io/get-pip.py
+python3.4 get-pip.py
+
+pip-3.4 install flask
+
 cd ../..
+
+rm -rf Python-3.4.3
+
